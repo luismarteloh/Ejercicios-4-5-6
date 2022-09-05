2db289393a782c34af1b817cace603096f525d8a
luismarteloh <luis.martelohernandez@gmail.com>
2022-09-05 10:24:51 -0500

    Arreglado el intervalo del render

--- src/component/functionClock.jsx
+++ src/component/functionClock.jsx
@@ -2,33 +2,33 @@ import React, { useEffect, useState } from 'react'
 
 export default function FunctionClock() {
     const [state, setState] = useState({
-
         fecha: new Date(),
         nombre: 'Martin',
         apellidos: 'San José',
         edad: 0,
     })
 
+    const newState = () => {
+      setState({
+        ...state,
+        fecha: new Date(),
+        edad: state.edad+1,
+      })
+    }
+
     useEffect(() => {
-
-        console.log('me actualizé')
-        setInterval(
-            () => setState({
-          ...state,
-          edad: state.edad+1,
-          fecha: new Date(),
-        }), 1000
-        )
-
-        
-      },);
+      console.log('actualizado')
+      const intervalAge = setInterval(()=>{
+        newState();
+      },1000);
+      return () => clearInterval(intervalAge);
+      });
 
   return (
     <div>
       <h2>Hora Actual: {state.fecha.toLocaleTimeString()}</h2>
       <h3>{state.nombre} {state.apellidos}</h3>
       <h1>Edad: {state.edad}</h1>
-      <h3>actualizando</h3>
     </div>
   )
 }
